797779fa30edfe36c15781694c583858d5560d59
“takumaN28” <okumura.t@nadia.bz>
2024-08-29 17:44:52 +0900

    [update]一旦そのまま入れてみる

--- app.js
+++ app.js
@@ -1,61 +1,191 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const API_KEY = "RGAPI-714f9c7a-873b-4fbf-9ba6-a419929b02e1";
+const platform = "jp1";
+const region = "asia";
+// const summonerName = "優しいゴーヤs";
+const summonerName = "ceros";
+// const tagline = "6106";
+const tagline = "111";
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+// 本当はダメらしいdbとか使ったほうがいいらしい
+let puuid; // puuidをグローバルスコープで定義
+let summonerId; // summonerIdをグローバルスコープで定義
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+
+// CORSを許可する
+app.use(cors());
+
+//postで情報を受け入れる
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3002");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTION"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+
+// サーバーを起動
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// // サーバーが起動した後にdataFromAPIを確認
+server.on('listening', async () => {
+    try {
+        // Riot APIにリクエストを送信
+        const response = await axios.get(`https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}?api_key=${API_KEY}`);
+        // puuidをグローバル変数にセット
+        puuid = response.data.puuid;
+
+        const summonerResponse = await axios.get(`https://${platform}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
+        // summonerIdをグローバル変数にセット
+        summonerId = summonerResponse.data.id;
+
+
+    } catch (error) {
+        console.log('Data from API has not been fetched yet.');
+    }
+});
+
+
+
+// Riot APIエンドポイントへのリクエストを処理するハンドラ
+app.get('/account', async (req, res) => {
+    try {
+        // Riot APIにリクエストを送信
+        const response = await axios.get(`https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}?api_key=${API_KEY}`);
+        // レスポンスをクライアントに返す
+        res.json(response.data);
+
+
+    } catch (error) {
+        // エラーが発生した場合はエラーメッセージを返す
+        res.status(500).json({
+            error: error.message
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    }
+});
+
+// POSTリクエストを処理するハンドラ
+app.post('/account', async (req, res) => {
+    try {
+        const { summonerName, tagline } = req.body; // リクエストボディからデータを取得
+        console.log("Received summonerName:", summonerName);
+        console.log("Received tagline:", tagline);
+
+        // Riot APIにリクエストを送信
+        const response = await axios.get(`https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}?api_key=${API_KEY}`);
+
+        // puuidをグローバル変数にセット
+        puuid = response.data.puuid;
+
+        // レスポンスをクライアントに返す
+        res.json(response.data);
+    } catch (error) {
+        // エラーが発生した場合はエラーメッセージを返す
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
+
+
+
+app.get('/account02', async (req, res) => {
+
+    /*
+    以下が含まれるAPI
+    id
+    accountId
+    puuid
+    profileIconId
+    revisionDate
+    summonerLevel
+    */
+
+    try {
+        // puuidが未定義の場合はエラーを返す
+        if (!puuid) {
+            throw new Error('puuid is not defined');
+        }
+
+        // Riot APIにリクエストを送信
+        const response = await axios.get(`https://${platform}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`);
+        res.json(response.data);
+
+
+    } catch (error) {
+        // エラーが発生した場合はエラーメッセージを返す
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
+
+
+app.get('/latestMatches', async (req, res) => {
+    try {
+        // puuidが未定義の場合はエラーを返す
+        if (!puuid) {
+            throw new Error('puuid is not defined');
+        }
+
+        // Riot APIにリクエストを送信
+        // 最新10件のmatchIds取得
+        const response = await axios.get(`https://${region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${API_KEY}`);
+        const matchIds = response.data;
+        const matches = [];
+
+        // matchIdsをループしてそれぞれの試合情報を取得
+        for (const matchId of matchIds) {
+            const matchResponse = await axios.get(
+                `https://${region}.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${API_KEY}`
+            );
+            matches.push(matchResponse.data);
+        }
+
+        // レスポンスをクライアントに返す
+        res.json(matches);
+
+    } catch (error) {
+        // エラーが発生した場合はエラーメッセージを返す
+
+    }
+});
+
+app.get('/leagueEntries', async (req, res) => {
+    try {
+        // puuidが未定義の場合はエラーを返す
+        if (!puuid) {
+            throw new Error('puuid is not defined');
+        }
+
+        // Riot APIにリクエストを送信
+        // 現在のリーグ（ランク）でのサモナー情報
+        const response = await axios.get(`https://${platform}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`);
+        const leagueData = response.data;
+
+        // レスポンスをクライアントに返す
+        res.json(leagueData);
+
+    } catch (error) {
+        // エラーが発生した場合はエラーメッセージを返す
+
+    }
+});
